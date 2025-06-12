f6fc444e5ce612e986f1f31fb2b4c64af319dd6e
ardiiiv <ardiiiv222@gmail.com>
2025-06-13 00:19:26 +0700

    deploy

--- src/views/QuizPage.vue
+++ src/views/QuizPage.vue
@@ -14,7 +14,7 @@ const handleSubmit = async () => {
   }
 
   try {
-    const response = await fetch('https://api-predict-eduwise-production.up.railway.app/predict', {
+    const response = await fetch('https://api-predict-eduwise-production.up.railway.app', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
